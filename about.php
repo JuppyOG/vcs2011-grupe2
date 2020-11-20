3c3b5ce4a8a5aad1e5ab552bd2cc4279ac36a819
Eregentqwerty <lskostas1@gmail.com>
2020-11-20 19:55:56 +0200

    about puslapio pradzia

--- about.php
+++ about.php
@@ -3,8 +3,54 @@
 
 
 
-<!-- Čia apie puslapio vidinė dalis -->
+<!-- <div class ="pirmas">
+<img src="https://picsum.photos/150" alt="">
+<p>
+Lukas Špelveris.Busimas programuotojas <a class="btn" href="">Issamiau</a>
+</p>
+<img src="https://picsum.photos/150" alt="">
+<p>
+Gabija Gasiunaite.Busima programuotoja <a class="btn" href="">Issamiau</a>
+</p>
+<img src="https://picsum.photos/150" alt="">
+<p>
+Rytis Urbonas.Busimas programuotojas <a class="btn" href="">Issamiau</a>
+</p>
+<img src="https://picsum.photos/150" alt="">
+<p>
+Edita.Busima programuotoja<a class="btn" href="">Issamiau</a>
+</p>
+</div> -->
 
+
+<div class="row">
+
+<div class="col s12 m6 l6 ">
+    <img src="https://picsum.photos/150" alt="">
+    <p>
+    Lukas Špelveris.Future programer <a class="btn-small" href="">Issamiau</a>
+    </p>
+</div>
+
+<div class="col s12 m6 l6 ">
+    <img src="https://picsum.photos/150" alt="">
+    <p>
+    Gabija Gasiunaite.Future programer <a class="btn-small" href="">Issamiau</a>
+    </p>
+</div>
+
+<div class="col s12 m6 l6 "><img src="https://picsum.photos/150" alt="">
+    <p>
+    Rytis Urbonas.Future programer <a class="btn-small" href="">Issamiau</a>
+    </p>
+</div>
+
+<div class="col s12 m6 l6 "><img src="https://picsum.photos/150" alt="">
+    <p>
+    Edita.Future programer <a class="btn-small" href="">Issamiau</a>
+    </p>
+</div>
+</div>
 
 
 <?php include "footer.php" ?>
