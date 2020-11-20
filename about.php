5598e37450ccffe290c9a424320418a67ae9e7e9
Eregentqwerty <lskostas1@gmail.com>
2020-11-20 21:28:10 +0200

    Sutvarkyti paveiksliukai

--- about.php
+++ about.php
@@ -7,28 +7,30 @@
     <div class="images">
 
 <div class="col s12 m6 l6 ">
-    <img src="images/the best.jpg" alt="">
+    <img src="images/boy.jpg" alt="">
     <p>
-    Lukas.Future programmer <a class="btn-small" href="">Issamiau</a>
+    Lukas.Future programmer <a class="btn-small" href="">More</a>
     </p>
 </div>
 
 <div class="col s12 m6 l6 ">
-    <img src="images/the best.jpg" alt="">
+    <img src="images/girl.jpg" alt="">
     <p>
-    Gabija.Future programmer <a class="btn-small" href="">Issamiau</a>
+    Gabija.Future programmer <a class="btn-small" href="">More</a>
     </p>
 </div>
 
-<div class="col s12 m6 l6 "><img src="images/the best.jpg" alt="">
+<div class="col s12 m6 l6 ">
+    <img src="images/boy2.jpg" alt="">
     <p>
-    Rytis.Future programmer <a class="btn-small" href="">Issamiau</a>
+    Rytis.Future programmer <a class="btn-small" href="">More</a>
     </p>
 </div>
 
-<div class="col s12 m6 l6 "><img src="images/the best.jpg" alt="">
+<div class="col s12 m6 l6 ">
+    <img src="images/girl2.jpg" alt="">
     <p>
-    Edita.Future programmer <a class="btn-small" href="">Issamiau</a>
+    Edita.Future programmer <a class="btn-small" href="">More</a>
     </p>
 </div>
 </div>
