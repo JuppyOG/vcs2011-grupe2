9feff980752ebb0c2df251298e53b2cf75cc22b7
gabijagasiunaite <gabija_gasinaite@yahoo.com>
2020-11-20 20:59:08 +0200

    beveikdone

--- index.php
+++ index.php
@@ -6,37 +6,39 @@
 <h1 class="blue lighten-4 black-text">
         In this page we are going to tell you our experience in Vilnius Coding School
     </h1>
-    <a  class= " waves-effect waves-light btn"  href="#"><i class="material-icons left">cloud
+    <a  class= " waves-effect waves-light btn"  href="#"><i class="material-icons left">
     </i>read more</a>
   </div>
 </div>
+
 <div class= "container">
 
-    <a  class= " waves-effect waves-light btn"  href="#"><i class="material-icons left">cloud
-    </i>read more</a>
+    
     <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore cum incidunt suscipit laboriosam animi facilis illum natus soluta, id eaque, porro velit doloremque, sequi recusandae delectus necessitatibus mollitia accusamus earum?</p>
+    </div>
+
     <div class="row">
-        <div class="col s12 m6 l3 sky blue"><img class= circle src="https://picsum.photos/100" alt="">
-            <h5>CSS</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos hic ducimus voluptate quia ullam culpa ea, earum quos explicabo delectus velit vel omnis ut sed dolore magni corporis at quam.</p>
-            <a  class= " waves-effect waves-light btn"  href="#"><i class="material-icons left">cloud
-    </i>more info</a>
+        <div class="col s12 m6 l4 "><img src= "images/phplogo.png" alt="php logo">
+            <h5>PHP</h5>
+            <p>PHP is a general-purpose scripting language especially suited to web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994. The PHP reference implementation is now produced by The PHP Group.</p>
+            <a  class= " waves-effect waves-light btn"  href="https://google.comm"><i class="material-icons left">
+    </i>read more</a>
             </div>
         
-            <div class="col s12 m6 l3 light blue"><img class= circle src="https://picsum.photos/100" alt="">
+            <div class="col s12 m6 l4"><img src="images/javalogo.png" alt="Java logo">
             <h5>Java</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos hic ducimus voluptate quia ullam culpa ea, earum quos explicabo delectus velit vel omnis ut sed dolore magni corporis at quam.</p>
-            <a  class= " waves-effect waves-light btn"  href="#"><i class="material-icons left">cloud
-    </i>more info</a>
+            <p>Java is a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible. It is a general-purpose programming language intended to let application developers write once, run anywhere (WORA), meaning that compiled Java code can run on all platforms that support Java without the need for recompilation.</p>
+            <a  class= " waves-effect waves-light btn"  href="https://google.com"><i class="material-icons left">
+    </i>read more</a>
             </div>
-            <div class="col s12 m6 l3 light blue"><img class= circle src="https://picsum.photos/100" alt="">
+            <div class="col s12 m6 l4"><img src="images/Clogo.png" alt="C# logo">
             <h5>C#</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos hic ducimus voluptate quia ullam culpa ea, earum quos explicabo delectus velit vel omnis ut sed dolore magni corporis at quam.</p>
-            <a  class= " waves-effect waves-light btn"  href="#"><i class="material-icons left">cloud
-    </i>more info</a>
+            <p>C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines.</p>
+            <a  class= " waves-effect waves-light btn"  href="https://google.com"><i class="material-icons left">
+    </i>read more</a>
             </div>
+
+
 </div>
-
-
 
 <?php include "footer.php" ?>
