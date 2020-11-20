eacea0b258e0df7c4aac1eab9e9a995b65ad9ef9
gabijagasiunaite <gabija_gasinaite@yahoo.com>
2020-11-20 21:32:18 +0200

    nauja foto

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <?php include "header.php" ?>
 <div class="fonas"> 
   <div class= "fono-tekstas container center-align" >
-  <h1>VCS team of Rytis, Edita, Lukas and Gabija webpage</h1>
-<h1 class="blue lighten-4 black-text">
+ 
+<h1 class="blue lighten-5 black-text">
         In this page we are going to tell you our experience in Vilnius Coding School
     </h1>
     <a  class= " waves-effect waves-light btn"  href="#"><i class="material-icons left">
@@ -14,7 +14,7 @@
 <div class= "container">
 
     
-    <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore cum incidunt suscipit laboriosam animi facilis illum natus soluta, id eaque, porro velit doloremque, sequi recusandae delectus necessitatibus mollitia accusamus earum?</p>
+    <p> in 8 weeks of programming classes we have learnt the following programming languages: bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
     </div>
 
     <div class="row">
@@ -22,20 +22,20 @@
             <h5>PHP</h5>
             <p>PHP is a general-purpose scripting language especially suited to web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994. The PHP reference implementation is now produced by The PHP Group.</p>
             <a  class= " waves-effect waves-light btn"  href="https://google.comm"><i class="material-icons left">
-    </i>read more</a>
+    </i>read more about PHP</a>
             </div>
         
             <div class="col s12 m6 l4"><img src="images/javalogo.png" alt="Java logo">
             <h5>Java</h5>
             <p>Java is a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible. It is a general-purpose programming language intended to let application developers write once, run anywhere (WORA), meaning that compiled Java code can run on all platforms that support Java without the need for recompilation.</p>
             <a  class= " waves-effect waves-light btn"  href="https://google.com"><i class="material-icons left">
-    </i>read more</a>
+    </i>read more about Java</a>
             </div>
             <div class="col s12 m6 l4"><img src="images/Clogo.png" alt="C# logo">
             <h5>C#</h5>
             <p>C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines.</p>
             <a  class= " waves-effect waves-light btn"  href="https://google.com"><i class="material-icons left">
-    </i>read more</a>
+    </i>read more about C#</a>
             </div>
 
 
