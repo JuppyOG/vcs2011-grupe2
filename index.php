7c5699b1a556ad5155f00c81e8e889d1a8293547
gabijagasiunaite <gabija_gasinaite@yahoo.com>
2020-11-23 21:33:21 +0200

    parallex

--- index.php
+++ index.php
@@ -1,47 +1,52 @@
-
 <?php include "header.php" ?>
-<div class= "pagrindinis-fonas">
+<div class="pagrindinis-fonas">
 
-<div class="fonas"> 
-  <div class= "fono-tekstas container center-align" >
- 
-<h1 class="blue lighten-5 black-text">
-        In this page we are going to tell you our experience in Vilnius Coding School
-    </h1>
-    <a  class= " waves-effect waves-light btn"  href="#"><i class="material-icons left">
-    </i>read more</a>
-  </div>
-</div>
+        <div class="fonas">
+                <div class="fono-tekstas container center-align">
 
-<div class= "container">
+                        <h1 class="blue lighten-5 black-text">
+                                In this page we are going to tell you our experience in Vilnius Coding School
+                        </h1>
+                        <a class=" waves-effect waves-light btn" href="#"><i class="material-icons left">
+                                </i>read more</a>
+                </div>
+        </div>
 
-    
-    <p> in 8 weeks of programming classes we have learnt the following programming languages: bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+        <div class="container">
+
+
+                <p> in 8 weeks of programming classes we have learnt the following programming languages: bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+        </div>
+
+        <div class="row center-align" >
+                <div class="col s12 m6 l4 ">
+                        <img class= "programavimo-kalbu-logo" src="images/phplogo.png" alt="php logo">
+                        <h5>PHP</h5>
+                        <p>PHP is  general-purpose scripting language especially suited to web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994. The PHP reference implementation is now produced by The PHP Group.</p>
+                        <a class=" waves-effect waves-light btn" href="https://google.comm"><i class="material-icons left">
+                                </i>read more about PHP</a>
+                </div>
+
+                <div class="col s12 m6 l4">
+                        <img class= "programavimo-kalbu-logo" src="images/javalogo.png" alt="Java logo">
+                        <h5>Java</h5>
+                        <p>Java is a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible. It is a general-purpose programming language intended to let application developers write once.</p>
+                        <a class=" waves-effect waves-light btn" href="https://google.com"><i class="material-icons left">
+                                </i>read more about Java</a>
+                </div>
+                <div class="col s12 m6 l4">
+                        <img class= "programavimo-kalbu-logo" src="images/Clogo.png" alt="C# logo">
+                        <h5>C#</h5>
+                        <p>C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines.</p>
+                        <a class=" waves-effect waves-light btn" href="https://google.com"><i class="material-icons left">
+                                </i>read more about C#</a>
+                </div>
+
+
+        </div> 
+        <div class="parallax-container">
+      <div class="parallax"><img src="https://picsum.photos/2000"></div>
     </div>
-
-    <div class="row">
-        <div class="col s12 m6 l4 "><img src= "images/phplogo.png" alt="php logo">
-            <h5>PHP</h5>
-            <p>PHP is a general-purpose scripting language especially suited to web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994. The PHP reference implementation is now produced by The PHP Group.</p>
-            <a  class= " waves-effect waves-light btn"  href="https://google.comm"><i class="material-icons left">
-    </i>read more about PHP</a>
-            </div>
-        
-            <div class="col s12 m6 l4"><img src="images/javalogo.png" alt="Java logo">
-            <h5>Java</h5>
-            <p>Java is a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible. It is a general-purpose programming language intended to let application developers write once.</p>
-            <a  class= " waves-effect waves-light btn"  href="https://google.com"><i class="material-icons left">
-    </i>read more about Java</a>
-            </div>
-            <div class="col s12 m6 l4"><img src="images/Clogo.png" alt="C# logo">
-            <h5>C#</h5>
-            <p>C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines.</p>
-            <a  class= " waves-effect waves-light btn"  href="https://google.com"><i class="material-icons left">
-    </i>read more about C#</a>
-            </div>
-
-
-</div>
 </div>
 
 <?php include "footer.php" ?>
