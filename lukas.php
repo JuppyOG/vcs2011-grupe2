19aa9a1b7dd8b26a275b128c1769f8eade984da6
Eregentqwerty <lskostas1@gmail.com>
2020-11-23 20:46:01 +0200

    pridetos korteles i lukas.php

--- lukas.php
+++ lukas.php
@@ -1,10 +1,39 @@
 <?php include "header.php" ?>
 
 <h1>Hello</h1>
-<div class="Lukas">
-<img src="images/defaultImageResized.jpg" alt="">
 
-<p>My name is Lukas Špelveris. I`m starting my carrier in programming. So far I learned basics of Java, C# and PHP </p>
+<div class="container">
+<div class="Lukas">
+<div class="row">
+    <div class="col s12 m6">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/defaultImageResized.jpg">
+          <span class="card-title red">About me</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">account_circle</i></a>
+        </div>
+        <div class="card-content">
+          <p>My name is Lukas Špelveris 27 y.o. male . I`m starting my carrier in programming. So far I learned basics of Java, C# and PHP</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/hobby.jpg">
+          <span class="card-title red">Hobbies</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">account_circle</i></a>
+        </div>
+        <div class="card-content">
+          <p>I like the outdoors, basketball, football, swimming, computer games... 
+              
+        </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
 </div>
+
 
 <?php include "footer.php" ?>
