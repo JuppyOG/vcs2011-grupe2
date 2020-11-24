015ff77b84f443ba2a9b45fb0d32ad439f641ef2
gabijagasiunaite <gabija_gasinaite@yahoo.com>
2020-11-24 18:36:50 +0200

    pakeitimai

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,7 @@
 
 
 /* Gabija stiliaus kodas: */
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
